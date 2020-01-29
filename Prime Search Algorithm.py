67fbc30fdfccf10393296d2b48d08594671c8f2b
Nerozarg <54819877+Nerozarg@users.noreply.github.com>
2020-01-30 00:40:05 +0100

    v1

--- Prime Search Algorithm.py
+++ Prime Search Algorithm.py
@@ -9,72 +9,69 @@ def prime(x):
         3: True,
         5: True
         }
-    loop, nextBase = primeHelper(primes, x)
-    return dictPrimeList(primes, loop, nextBase)
+    listOfPrimes, nextBase, primePosInList_next = primeHelper(primes, x)
+    return primeRemoveMultiplesAll(primes, listOfPrimes, nextBase, primePosInList_next)
 
 
-# Removes non prime integers through a pattern
-def blackPrimeList(primes, baseValue, primesList, nextBase, main=True):
-    for i in range(baseValue, len(primesList)):
-        testing = primesList[baseValue]*primesList[i]
-        if testing > nextBase:
-            break
-        try:
-            del primes[testing]
-        except Exception:
-            pass
-        if main:
-            blackPrimeList(primes, i+1, primesList, nextBase, False)
+# Deletes all multiples of all primes
+def primeRemoveMultiplesAll(primes, listOfPrimes, nextBase, primePosInList):
+    state = True
+    while state:
+        state = primeRemoveMultiples(primes, listOfPrimes, nextBase, primePosInList)
+        primePosInList += 1
+    return primeDictToList(primes)
 
 
-# Creates a sorted array from Object
-def dictPrimeList(primes, loop, nextBase):
-    primesList = [1, 2, 3, 5]
-    primesLen = nextBase
-    for x in range(5, primesLen, 2):
-        if not x == 5:
+# Deletes all multiples of a single prime
+def primeRemoveMultiples(primes, listOfPrimes, nextBase, primePosInList):
+    if listOfPrimes[primePosInList]**2 > nextBase:
+        # Unable to remove any more multiples
+        return False
+    for i in range(primePosInList, len(listOfPrimes)):
+        valueToRemove = listOfPrimes[primePosInList]*listOfPrimes[i]
+        if valueToRemove <= nextBase:
             try:
-                if primes[x]:
-                    primesList.append(x)
+                del primes[valueToRemove]
             except Exception:
+                # Expected, since listOfPrimes contains deleted non primes
                 pass
-    return primesList
+        else:
+            # Able to remove multiples
+            return True
+
+
+# Returns sorted, low to high, prime list
+def primeDictToList(primes):
+    return sorted(list(dict.keys(primes)))
 
 
 # Main function Helper
 def primeHelper(primes, loops):
     loop = 0
-    primesList = dictPrimeList(primes, loop, 30)
-    baseValue = [primesList[1], primesList[2]]
+    listOfPrimes = primeDictToList(primes)
+    primesInBase = [listOfPrimes[1], listOfPrimes[2]]
 
     while True:
-        currentBase = prod(baseValue)
-        nextBase = currentBase * primesList[len(baseValue)+1]
-
-        # Last prime appended during prior run
-        for x in range(len(primes)-1, -1, -1):
-            if primesList[x] < currentBase:
-                lastPosition = x
-                break
+        currentBase = prod(primesInBase)
+        nextBase = currentBase * listOfPrimes[len(primesInBase)+1]
 
         # Make potential primes
         for base in range(currentBase, nextBase, currentBase):
-            for prime in primesList[:lastPosition+1]:
-                if prime in baseValue:
+            for prime in listOfPrimes[:len(listOfPrimes)]:
+                if prime in primesInBase:
                     continue
                 primes[base+prime] = True
 
-        # Correct potential primes
-        primesList = dictPrimeList(primes, loop, nextBase)
-        blackPrimeList(primes, len(baseValue)+1, primesList, nextBase)
-        primesList = dictPrimeList(primes, loop, nextBase)
+        # Delete next prime multiples
+        listOfPrimes = primeDictToList(primes)
+        primeRemoveMultiples(primes, listOfPrimes, nextBase, len(primesInBase)+1)
+        listOfPrimes = primeDictToList(primes)
 
         # End section
         loop += 1
         if loop >= loops:
-            return loop, nextBase
-        baseValue.append(primesList[len(baseValue)+1])
+            return listOfPrimes, nextBase, len(primesInBase)+2
+        primesInBase.append(listOfPrimes[len(primesInBase)+1])
 
 
-# test
-# print(prime(4))
+print(prime(4))
