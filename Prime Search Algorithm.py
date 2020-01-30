732e4cd7287263b75282566417c6bb64c6442994
Nerozarg <54819877+Nerozarg@users.noreply.github.com>
2020-01-30 18:46:31 +0100

    v1.1 - Class Prime

--- Prime Search Algorithm.py
+++ Prime Search Algorithm.py
@@ -1,77 +1,87 @@
 from numpy import prod
 
 
-# Finds primes, x equals how many loops
-def prime(x):
-    primes = {
-        1: True,
-        2: True,
-        3: True,
-        5: True
-        }
-    listOfPrimes, nextBase, primePosInList_next = primeHelper(primes, x)
-    return primeRemoveMultiplesAll(primes, listOfPrimes, nextBase, primePosInList_next)
+class Prime:
+    # Variables
+        # (Dict) primes
+        # (Arr) listOfPrimes
+        # (Arr) primesInBase
+        # (Int) currentBase
+        # (Int) nextBase
+        # (Int) loop
+    def __init__(self):
+        self.primes = {
+            1: True,
+            2: True,
+            3: True,
+            5: True
+            }
+        self.loop = 0
+        self.listOfPrimes = self.primeDictToList()
+        self.primesInBase = [self.listOfPrimes[1], self.listOfPrimes[2]]
+
+    # Returns Prime Dictionary as Array
+    def primeDictToList(self):
+        return sorted(list(dict.keys(self.primes)))
+
+    # Deletes all multiples of a single prime
+    def primeRemoveMultiples(self, primePosInList):
+        if self.listOfPrimes[primePosInList]**2 > self.nextBase:
+            # Unable to remove any more multiples
+            return False
+        for i in range(primePosInList, len(self.listOfPrimes)):
+            valueToRemove = self.listOfPrimes[primePosInList]*self.listOfPrimes[i]
+            if valueToRemove <= self.nextBase:
+                try:
+                    del self.primes[valueToRemove]
+                except Exception:
+                    # Expected, since listOfPrimes contains deleted non primes
+                    pass
+            else:
+                # Able to remove multiples
+                return True
+
+    # Deletes all multiples of all primes
+    def primeRemoveMultiplesAll(self, primePosInList):
+        state = True
+        while state:
+            state = self.primeRemoveMultiples(primePosInList)
+            primePosInList += 1
+        return self.primeDictToList()
+
+    # Main Search Function
+    def primeSearch(self, loops):
+        while True:
+            self.currentBase = prod(self.primesInBase)
+            self.nextBase = self.currentBase * self.listOfPrimes[len(self.primesInBase)+1]
+
+            # Make potential primes
+            for base in range(self.currentBase, self.nextBase, self.currentBase):
+                for prime in self.listOfPrimes[:len(self.listOfPrimes)]:
+                    if prime in self.primesInBase:
+                        continue
+                    self.primes[base+prime] = True
+
+            # Delete next prime multiples
+            self.listOfPrimes = self.primeDictToList()
+            self.primeRemoveMultiples(len(self.primesInBase)+1)
+            self.listOfPrimes = self.primeDictToList()
+
+            # End section
+            self.loop += 1
+            if self.loop >= loops:
+                self.listOfPrimes = self.primeRemoveMultiplesAll(len(self.primesInBase)+2)
+                return self.listOfPrimes
+            self.primesInBase.append(self.listOfPrimes[len(self.primesInBase)+1])
 
 
-# Deletes all multiples of all primes
-def primeRemoveMultiplesAll(primes, listOfPrimes, nextBase, primePosInList):
-    state = True
-    while state:
-        state = primeRemoveMultiples(primes, listOfPrimes, nextBase, primePosInList)
-        primePosInList += 1
-    return primeDictToList(primes)
+PSO = Prime()
 
+print("Prime array: " + str(PSO.primeSearch(4)))
+print("Prime dictionary: " + str(PSO.primes))
+print("Primes in base: " + str(PSO.primesInBase))
+print("Current base: " + str(PSO.currentBase))
+print("Next base: " + str(PSO.nextBase))
+print("Loop: " + str(PSO.loop))
 
-# Deletes all multiples of a single prime
-def primeRemoveMultiples(primes, listOfPrimes, nextBase, primePosInList):
-    if listOfPrimes[primePosInList]**2 > nextBase:
-        # Unable to remove any more multiples
-        return False
-    for i in range(primePosInList, len(listOfPrimes)):
-        valueToRemove = listOfPrimes[primePosInList]*listOfPrimes[i]
-        if valueToRemove <= nextBase:
-            try:
-                del primes[valueToRemove]
-            except Exception:
-                # Expected, since listOfPrimes contains deleted non primes
-                pass
-        else:
-            # Able to remove multiples
-            return True
-
-
-# Returns sorted, low to high, prime list
-def primeDictToList(primes):
-    return sorted(list(dict.keys(primes)))
-
-
-# Main function Helper
-def primeHelper(primes, loops):
-    loop = 0
-    listOfPrimes = primeDictToList(primes)
-    primesInBase = [listOfPrimes[1], listOfPrimes[2]]
-
-    while True:
-        currentBase = prod(primesInBase)
-        nextBase = currentBase * listOfPrimes[len(primesInBase)+1]
-
-        # Make potential primes
-        for base in range(currentBase, nextBase, currentBase):
-            for prime in listOfPrimes[:len(listOfPrimes)]:
-                if prime in primesInBase:
-                    continue
-                primes[base+prime] = True
-
-        # Delete next prime multiples
-        listOfPrimes = primeDictToList(primes)
-        primeRemoveMultiples(primes, listOfPrimes, nextBase, len(primesInBase)+1)
-        listOfPrimes = primeDictToList(primes)
-
-        # End section
-        loop += 1
-        if loop >= loops:
-            return listOfPrimes, nextBase, len(primesInBase)+2
-        primesInBase.append(listOfPrimes[len(primesInBase)+1])
-
-
-print(prime(4))
+# Currently unable to continue search, because all line constructing non primes are removed
